5d677f30ba911bab4dcc7ff15df7b22387acfc17
Kristin Ortiz <itskristin.ortiz@gmail.com>
2017-12-13 16:26:01 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,4 +1,4 @@
-#def using_while
+def using_while
 	#levitation_force = 6
 	#while levitation_force <=10
 	#	levitation_force +=1
@@ -11,4 +11,5 @@ counter = 6
 until counter == 10
   puts "Wingardium Leviosa"
   counter +=1
+end
 end
